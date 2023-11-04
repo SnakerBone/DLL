eb2aed2cef97e4fea5dae3a86c0f5b0b2b8753a0
SnakerBone <raxxtq@gmail.com>
2023-11-04 19:50:10 +1100

    DLL Troubleshooting...

--- src/main/java/xyz/snaker/natives/SnakerNatives.java
+++ src/main/java/xyz/snaker/natives/SnakerNatives.java
@@ -44,7 +44,7 @@ public class SnakerNatives
                 throw new IOException("Could not make path absolute: " + path);
             }
 
-            System.loadLibrary("snkr");
+            System.loadLibrary("snkr.dll");
         } catch (Exception e) {
             LOGGER.errorf("Could not load libraries for SnakerNatives: []", e.getMessage());
             return;
